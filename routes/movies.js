12e6deccef492c061620470ec4dd57fdcc432073
HelenHMaher <helenharlanmaher@gmail.com>
2020-12-01 10:58:18 +0100

    begining of toSwipe

--- routes/movies.js
+++ routes/movies.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const router = express.Router();
 const MovieList = require('../models/movieList');
+const LikeTracker = require('../models/likeTracker');
+const Friends = require('../models/friends');
 const ensureAuthenticated = require('../middleware/ensureAuthenticated');
 const axios = require('axios');
 
@@ -107,6 +109,81 @@ router.get('/filterNames', (req, res) => {
   });
 });
 
-router.get('/toSwipe', ensureAuthenticated, (req, res) => {});
+router.get('/toSwipe', ensureAuthenticated, (req, res) => {
+  LikeTracker.findOne({ userId: req.user.id }, async (err, doc) => {
+    try {
+      if (err) throw err;
+      const filters = doc ? doc['filters'] : {};
+      Friends.find(
+        {
+          $and: [
+            { accepted: true },
+            {
+              $or: [
+                { senderUserId: req.user.id },
+                { receiverUserId: req.user.id },
+              ],
+            },
+          ],
+        },
+        async (err, doc) => {
+          try {
+            if (err) throw err;
+            const friendsArray = !doc
+              ? []
+              : doc.map((x) => {
+                  if (x.senderUserId === req.user.id) {
+                    return x.receiverUserId;
+                  } else return x.senderUserId;
+                });
+            let toSwipe = [];
+            for (let i; i < friendsArray.length; i++) {
+              LikeTracker.findOne(
+                { userId: friendsArray[i] },
+                async (err, doc) => {
+                  try {
+                    if (err) throw err;
+                    if (doc) {
+                      const likes = doc['likes'].map((x) => x['film']);
+                      toSwipe = [...toSwipe, ...likes];
+                    }
+                  } catch (err) {
+                    console.log(err);
+                  }
+                }
+              );
+            }
+            for (let i; i < filters.length; i++) {
+              MovieList.findOne({ filterName: filter[i] }, async (err, doc) => {
+                try {
+                  if (err) throw err;
+                  if (doc) {
+                    toSwipe = [...toSwipe, ...doc[films]];
+                  }
+                } catch (err) {
+                  console.log(err);
+                }
+              });
+            }
+            let uniqueToSwipe = [];
+            while (toSwipe.length > 0) {
+              const movie = toSwipe.shift();
+              if (toSwipe.indexOf(movie) > -1) {
+                uniqueToSwipe.push(movie);
+              }
+            }
+
+            //GET LIKES AND DISLIKES ARRAY AND REMOVE THEM FROM UNIQUETOSWIPE
+            //TAKE 100 RANDOM ENTRIES AND SEND TO FRONTEND
+          } catch (error) {
+            console.log(error);
+          }
+        }
+      );
+    } catch (err) {
+      console.log(err);
+    }
+  });
+});
 
 module.exports = router;
