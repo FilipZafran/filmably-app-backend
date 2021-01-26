16d984a615c6c8268ff82c87076c8f27ce385221
Helen_Maher <helenhmaher@gmail.com>
2021-01-26 23:38:26 +0100

    testing for matches

--- routes/matches.js
+++ routes/matches.js
@@ -10,56 +10,36 @@ const ensureAuthenticated = require('../middleware/ensureAuthenticated');
 router.post('/oneFilm', ensureAuthenticated, (req, res) => {
   const film = req.body.film.filmId;
   const filmObject = req.body.film;
-  //fetch a list of all friends
-  Friends.find(
-    {
-      $and: [
-        { accepted: true },
-        {
-          $or: [{ senderUSerId: req.user.id }, { receiverUserId: req.user.id }],
-        },
-      ],
-    },
-    async (err, doc) => {
-      try {
-        if (err) throw err;
-        const friendsList = doc.map((x) => {
-          if (x.senderUserId.toString() === req.user.id) {
-            return x.receiverUserId;
-          } else return x.senderUserId;
-        });
-        //fetch likeTracker for friends and filter for filmId
-        LikeTracker.find({ userId: { $in: friendsList } }, async (err, doc) => {
-          try {
-            if (err) throw err;
-            const matchesList = doc.map((x) => {
-              x.likes.includes(film) ? x.userId : null;
-            });
-            //fetch username of friends that liked the film
-            User.find({ _id: { $in: matchesList } }, async (err, doc) => {
-              try {
-                if (err) throw err;
-                const fullMatchesArray = doc.map((user) => {
-                  return { id: user._id, username: user.username };
-                });
-                res.send({
-                  msg: 'matches list',
-                  movie: filmObject,
-                  matches: fullMatchesArray,
-                });
-              } catch (error) {
-                console.log(error);
-              }
-            });
-          } catch (error) {
-            console.log(error);
-          }
-        });
-      } catch (error) {
-        console.log(error);
-      }
+  const friendsList = req.body.allFriends;
+  console.log('friendsList: ', friendsList);
+  //fetch likeTracker for friends and filter for filmId
+  LikeTracker.find({ userId: { $in: friendsList } }, async (err, doc) => {
+    try {
+      if (err) throw err;
+      const matchesList = doc.map((x) => {
+        x.likes.includes(film) ? x.userId : null;
+      });
+      console.log('matchlist: ', matchList);
+      //fetch username of friends that liked the film
+      User.find({ _id: { $in: matchesList } }, async (err, doc) => {
+        try {
+          if (err) throw err;
+          const fullMatchesArray = doc.map((user) => {
+            return { id: user._id, username: user.username };
+          });
+          res.send({
+            msg: 'matches list',
+            movie: filmObject,
+            matches: fullMatchesArray,
+          });
+        } catch (error) {
+          console.log(error);
+        }
+      });
+    } catch (error) {
+      console.log(error);
     }
-  );
+  });
 });
 
 //GET a list of all movies liked by friends sorted from most to least likes
