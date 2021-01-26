4a410483064189107bd4de183ba4d1488c4edf48
Helen_Maher <helenhmaher@gmail.com>
2021-01-27 00:45:13 +0100

    successfully returning my shit

--- routes/matches.js
+++ routes/matches.js
@@ -8,33 +8,39 @@ const ensureAuthenticated = require('../middleware/ensureAuthenticated');
 //GET the friends who also like a specific movie
 
 router.post('/oneFilm', ensureAuthenticated, (req, res) => {
-  const film = req.body.film.filmId;
-  const filmObject = req.body.film;
+  const film = req.body.film;
   const friendsList = req.body.allFriends;
-  console.log('friendsList: ', friendsList);
+  const friendIdArray = friendsList.map((x) => x.id);
+  console.log('film: ', film);
+  // console.log('friendsList: ', friendsList);
   //fetch likeTracker for friends and filter for filmId
-  LikeTracker.find({ userId: { $in: friendsList } }, async (err, doc) => {
+  LikeTracker.find({ userId: { $in: friendIdArray } }, async (err, doc) => {
     try {
       if (err) throw err;
-      const matchesList = doc.map((x) => {
-        x.likes.includes(film) ? x.userId : null;
-      });
-      console.log('matchlist: ', matchList);
-      //fetch username of friends that liked the film
-      User.find({ _id: { $in: matchesList } }, async (err, doc) => {
-        try {
-          if (err) throw err;
-          const fullMatchesArray = doc.map((user) => {
-            return { id: user._id, username: user.username };
-          });
-          res.send({
-            msg: 'matches list',
-            movie: filmObject,
-            matches: fullMatchesArray,
-          });
-        } catch (error) {
-          console.log(error);
-        }
+      // console.log('doc: ', doc);
+      const matchesList = doc
+        .map((userLikeTracker) => {
+          const like = userLikeTracker.likes.filter(
+            (x) => x.film.id === film.id
+          );
+          // console.log('like: ', like);
+          if (like.length > 0) {
+            return userLikeTracker.userId;
+          } else {
+            return 'false';
+          }
+        })
+        .filter((x) => x !== 'false');
+      console.log('matchlist: ', matchesList);
+      //get username from userid
+      const matchesListWithNames = matchesList
+        .map((match) => friendsList.filter((friend) => friend.id === match))
+        .flat();
+      console.log('matchesListWithNames: ', matchesListWithNames);
+      res.send({
+        msg: 'matches list',
+        movie: film,
+        matches: matchesListWithNames,
       });
     } catch (error) {
       console.log(error);
