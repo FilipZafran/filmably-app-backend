32282adcadbaffdf9a699a174597ac4cce0079f2
Helen_Maher <helenhmaher@gmail.com>
2021-03-02 14:17:35 +0100

    remove console.logs

--- routes/matches.js
+++ routes/matches.js
@@ -11,8 +11,8 @@ router.post('/oneFilm', ensureAuthenticated, (req, res) => {
   const film = req.body.film;
   const friendsList = req.body.allFriends;
   const friendIdArray = friendsList.map((x) => x.id);
-  console.log('film: ', film);
-  console.log('friendsList: ', friendsList);
+  // console.log('film: ', film);
+  // console.log('friendsList: ', friendsList);
   //fetch likeTracker for friends and filter for filmId
   LikeTracker.find({ userId: { $in: friendIdArray } }, async (err, doc) => {
     try {
@@ -23,7 +23,7 @@ router.post('/oneFilm', ensureAuthenticated, (req, res) => {
           const like = userLikeTracker.likes.filter(
             (x) => x.film && x.film.id === film.id
           );
-          console.log('like: ', like);
+          // console.log('like: ', like);
           if (like.length > 0) {
             return userLikeTracker.userId;
           } else {
@@ -36,7 +36,7 @@ router.post('/oneFilm', ensureAuthenticated, (req, res) => {
       const matchesListWithNames = matchesList
         .map((match) => friendsList.filter((friend) => friend.id === match))
         .flat();
-      console.log('matchesListWithNames: ', matchesListWithNames);
+      // console.log('matchesListWithNames: ', matchesListWithNames);
       res.send({
         msg: 'matches list',
         movie: film,
